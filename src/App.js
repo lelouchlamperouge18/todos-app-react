656a997875c81392af047e2e7ca01900b3d24760
CongSonHoang <congsonhoang0810>
2020-09-04 16:47:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props); //mac dinh constructor
+    this.state={
+      items:[],
+      newItem: {
+        text: ''
+      }
+    }
+    this.addItem = this.addItem.bind(this); //lenh nay de su dung duoc setState
+    this.onChange = this.onChange.bind(this);
+  }
+  addItem(k){
+    k.preventDefault();    //Ko cho gui form??? di voi button
+    const newItem1 = this.state.newItem; //Item1 nay la 1 item moi
+    if(newItem1.text!==""){
+      const newItems = [...this.state.items, newItem1]; //mang items - them item moi vao cuoi neu no ko rong
+      this.setState({     
+        items: newItems,  //tra ve lai mang co them phan tu moi
+        newItem: {        //enter xong thi se reset lai form
+          text: ''
+        }
+      })
+    }
+    console.log(newItem1);
+  }
+  onChange(k){
+    this.setState({
+      newItem:{
+        text: k.target.value //khi nhap vao form thi se thay doi value cua newItem
+      }
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <form id="todos-form" onSubmit={this.addItem}>      {/*event in React*/}
+            <input type="text" placeholder="Input task.." 
+              value={this.state.newItem.text} onChange={this.onChange}/>
+            <button type="submit"> ADD TASK </button>
+          </form>
+        </header>
+        <ListItems items={this.state.items}></ListItems> {/*Hien thi cac items o ben duoi form*/}
+      </div>
+    )
+  }
 }
 
 export default App;
+/*Refs: https://reactjs.org/docs/react-component.html
+https://tuantranict.blogspot.com/2018/11/tao-ung-dung-todo-list-on-gian-bang.html*/
---
+++ src/ListItems.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ListItems(props){   //Phai dat ten function giong ten file
+    const listItems = props.items.map(item =>{     //Moi item tra ve cai text cua no
+        return (
+            <div className="list">
+                <p> {item.text} ➜    
+                    <button type="submit" id="button1"> COMPLETE </button>
+                    <button type="submit" id="button2"> DELETE </button>
+                </p>
+            </div>    
+        )
+    })
+    return(
+        <div>{listItems}</div>
+    )
+}
+export default ListItems;
