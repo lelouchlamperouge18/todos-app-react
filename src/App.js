88c74d35bc73b9d0c579815957121a7426d6dd51
CongSonHoang <congsonhoang0810>
2020-09-11 09:21:10 +0700

    update temp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ListItems from './ListItems'
 
@@ -12,14 +12,15 @@ class App extends React.Component{
         text: ''
       }
     }
-    this.addItem = this.addItem.bind(this); //lenh nay de su dung duoc setState
-    this.onChange = this.onChange.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
+    // this.addItem = this.addItem.bind(this); 
+    // this.onChange = this.onInputChange.bind(this);
+    // this.deleteItem = this.deleteItem.bind(this);
+    // this.completeItem = this.completeItem.bind(this);
   }
-  addItem(k){
-    k.preventDefault();    //Ko cho gui form??? di voi button| KHONG CHO RELOAD TRANG WEB
+  addItem = e => {
+    e.preventDefault();    //Ko cho gui form??? di voi button| KHONG CHO RELOAD TRANG WEB
     const newItem1 = this.state.newItem; //Item1 nay la 1 item moi
-    if(newItem1.text!==""){
+    if(newItem1.text!==""&&([...this.state.items].includes(newItem1.text)===false)){
       const newItems = [...this.state.items, newItem1]; //mang items - them item moi vao cuoi neu no ko rong
       this.setState({     
         items: newItems,  //tra ve lai mang co them phan tu moi
@@ -29,21 +30,43 @@ class App extends React.Component{
       })
     }
     console.log(newItem1);
-  }
-  onChange(k){
+  };
+  onInputChange = e =>{ 
     this.setState({
       newItem:{
-        text: k.target.value //khi nhap vao form thi se thay doi value cua newItem
+        text: e.target.value //khi nhap vao form thi se thay doi value cua newItem
       }
     })
-  }
-  deleteItem(k){
+  };
+  deleteItem = e =>{
     const afterDelete = this.state.items.filter(
-      item => item.text!==k
+      item => item.text!==e
     );
     this.setState({
       items: afterDelete
     })
+  };
+  completeItem = e => {
+    // const afterComplete = this.state.items.map(
+    //   item => { 
+    //     if (item.text===k) return (
+    //       <div className="list">
+    //         <p> <strike> {item.text} </strike> </p>
+    //       </div>   
+    //     ); else return item;
+    //   }
+    // );
+    // --------------------------------------------------
+    // const afterComplete = this.state.items.map(
+    //   item => { 
+    //     return (
+    //       <div> 123 </div>
+    //     )
+    //   }
+    // );
+    // this.setState({
+    //   items: afterComplete
+    // })
   }
   render(){
     return (
@@ -51,11 +74,12 @@ class App extends React.Component{
         <header>
           <form id="todos-form" onSubmit={this.addItem}>      {/*event in React*/}
             <input type="text" placeholder="Input task.." 
-              value={this.state.newItem.text} onChange={this.onChange}/>
+              value={this.state.newItem.text} onChange={this.onInputChange}/>
             <button type="submit"> ADD TASK </button>
           </form>
         </header>
-        <ListItems items={this.state.items} deleteItem={this.deleteItem}></ListItems> {/*Hien thi cac items o ben duoi form*/}
+        <ListItems items={this.state.items} deleteItem={this.deleteItem} 
+          completeItem={this.completeItem}></ListItems> {/*Hien thi cac items o ben duoi form*/}
       </div>
     )
   }
--- src/ListItems.js
+++ src/ListItems.js
@@ -4,10 +4,9 @@ function ListItems(props){   //Phai dat ten function giong ten file
     const listItems = props.items.map(item =>{     //Moi item tra ve cai text cua no cung 2 button
         return (
             <div className="list">
-                <p> {item.text} ➜    
-                    {/* <button type="radio"> COMPLETE </button> */}
-                    <button type="submit" onClick={()=>props.deleteItem(item.text)}> DELETE </button>
-                </p>
+                <p> {item.text} ➜ </p>   
+                <button type="submit" onClick={()=>props.completeItem(item.text)}> COMPLETE </button>
+                <button type="submit" onClick={()=>props.deleteItem(item.text)}> DELETE </button>
             </div>    
         )
     })
